6e0527b4c25cae0f197ab4d36c92ff58a9412275
Snowy <53881173+SnowyXS@users.noreply.github.com>
2026-07-12 22:18:00 +0300

    fixed missing bindableevents library

--- Libraries/UI/Library.lua
+++ Libraries/UI/Library.lua
@@ -4,6 +4,7 @@ local Base = {}
 Base.__index = Base
 
 local screengui = Instance.new("ScreenGui", game.CoreGui)
+local BindableEvents = loadfile("Aster/Libraries/Dependencies/BindableEvents.lua")()
 
 do
 	function Base:create_button(title) 
@@ -11,13 +12,13 @@ do
 		local button = {
 			Window = Window,
 		}
-		
+
 		local bindable = BindableEvents:Create()
 		local objects = self.objects
 
 		local row = Library.create_row(self.category_frame, title)
 		button.instance = row
-		
+
 		function button:on_changed(func)
 			bindable:Connect(func)
 		end
@@ -25,7 +26,7 @@ do
 		function button:fire(...)
 			bindable:Fire(...)
 		end
-		
+
 		function button:click()
 			local window = self.Window
 			local linked_category = self.linked_category
@@ -36,7 +37,7 @@ do
 				bindable:Fire()
 			end
 		end
-		
+
 		function button:set_category(category)
 			local image_label = Instance.new("ImageLabel", row)
 			image_label.AnchorPoint = Vector2.new(1, 0.5)
@@ -44,17 +45,17 @@ do
 			image_label.Position = UDim2.new(1, -5, 0.5, 1)
 			image_label.Size = UDim2.new(0, 25, 0, 25)
 			image_label.Image = "rbxassetid://133800611596665"
-			
+
 			button.linked_category = category
 		end
-		
+
 		table.insert(objects, button)
-		
+
 		Library.init_highlight(self, row)
 
 		self.size = self.size + 1
 		Window:resize(self)
-		
+
 		return button
 	end
 
@@ -70,16 +71,16 @@ do
 
 		local row = Library.create_row(self.category_frame, title)
 		toggle.instance = row
-		
+
 		local toggle_status = Instance.new("Frame", row)
 		toggle_status.AnchorPoint = Vector2.new(1, 0.5)
 		toggle_status.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
 		toggle_status.Position = UDim2.new(1, -12, 0.5, 1)
 		toggle_status.Size = UDim2.new(0, 10, 0, 10)
-		
+
 		local UICorner = Instance.new("UICorner", toggle_status)
 		UICorner.CornerRadius = UDim.new(0, 90)
-		
+
 		function toggle:on_changed(func)
 			bindable:Connect(func)
 		end
@@ -92,20 +93,20 @@ do
 			self.Value = not self.Value
 
 			toggle_status.BackgroundColor3 = self.Value and Color3.fromRGB(18, 255, 1) or Color3.fromRGB(255, 0, 4)
-			
+
 			bindable:Fire(self.Value)
 		end
 
 		table.insert(objects, toggle)
-		
+
 		Library.init_highlight(self, row)
-		
+
 		self.size = self.size + 1
 		Window:resize(self)
-		
+
 		return toggle
 	end
-	
+
 
 	function Base:create_slider(title, options) 
 		local Window = self.Window
@@ -134,7 +135,7 @@ do
 		slider_value.TextXAlignment = Enum.TextXAlignment.Right
 		slider_value.Position = UDim2.new(1, -6, 0.5, 1)
 		slider_value.Size = UDim2.new(1, 0, 0, 35)
-		
+
 		function slider:on_changed(func)
 			bindable:Connect(func)
 		end
@@ -147,7 +148,7 @@ do
 			local value = math.clamp(self.Value + direction * self.Increment, self.Min, self.Max)
 			local decimals = math.max(0, -math.floor(math.log10(self.Increment)))
 			value = math.floor(value * (10 ^ decimals) + 0.5) / (10 ^ decimals)
-			
+
 			self.Value = value
 			slider_value.Text = `< {value}{self.Prefix} >`
 			bindable:Fire(value)
@@ -162,13 +163,19 @@ do
 
 		return slider
 	end
-	
+
+	function Base:create_category(title)
+		local Window = self.Window
+		local category = Window:create_category(title)
+		self:create_button(title):set_category(category)
+	end
+
 	function Base:move(amount)
 		local objects = self.objects
 		local count = #objects
 		local old_selected = self.selected
 		if not old_selected or #objects <= 1 then return end
-		
+
 		self.selected = self.selected + amount
 
 		if self.selected > count then
@@ -176,7 +183,7 @@ do
 		elseif self.selected < 1 then
 			self.selected = count
 		end
-			
+
 		local current_object = objects[self.selected].instance
 		Library.set_properties(current_object, {
 			TextColor3 = Color3.fromRGB(255, 255, 255),
@@ -191,15 +198,15 @@ do
 			BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 		})
 	end
-	
+
 	function Base:click()
 		local obj = self.objects[self.selected]
-		
+
 		if obj and obj.click then
 			obj:click()
 		end
 	end
-	
+
 	function Base:step(direction)
 		local obj = self.objects[self.selected]
 
@@ -212,7 +219,7 @@ end
 function Library:create_window(title, icon, version)
 	local categories = {}
 	local opened_categories = {}
-	
+
 	local Window = {
 		categories = categories,
 		opened_categories = opened_categories,
@@ -224,20 +231,20 @@ function Library:create_window(title, icon, version)
 	main_frame.Position = UDim2.new(0, 24, 0, 24)
 	main_frame.Size = UDim2.new(0, 300, 0, 70)
 	main_frame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-	
+
 	Window.window_frame = main_frame
-	
+
 	Instance.new("UICorner", main_frame)
 
 	local top_bar = Instance.new("Frame", main_frame)
 	top_bar.Name = "top_bar"
 	top_bar.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
 	top_bar.Size = UDim2.new(0, 300, 0, 35)
-	
+
 	local ui_corner = Instance.new("UICorner", top_bar)
 	ui_corner.BottomLeftRadius = UDim.new(0, 0)
 	ui_corner.BottomRightRadius = UDim.new(0, 0)
-	
+
 	local title_label = Instance.new("TextLabel", top_bar)
 	title_label.Name = "title"
 	title_label.Text = title
@@ -272,7 +279,7 @@ function Library:create_window(title, icon, version)
 	local ui_corner = Instance.new("UICorner", bottom_bar)
 	ui_corner.TopRightRadius = UDim.new(0, 0)
 	ui_corner.TopLeftRadius = UDim.new(0, 0)
-	
+
 	local version_label = Instance.new("TextLabel", bottom_bar)
 	version_label.Name = "version"
 	version_label.Text = version
@@ -303,49 +310,49 @@ function Library:create_window(title, icon, version)
 		function category:SetVisible(bool)
 			category_frame.Visible = bool
 		end
-		
+
 		if #categories == 0 then
 			Window.current_category = category
 			table.insert(opened_categories, category)
 		end
-		
+
 		table.insert(categories, category)
 
 		return category
 	end
-	
+
 	function Window:open_category(category)
 		local current_category = Window.current_category
-		
+
 		current_category:SetVisible(false)
 		category:SetVisible(true)
-		
+
 		Window.current_category = category
-		
+
 		Window:resize(category)
 		table.insert(opened_categories, category)
 	end
-	
+
 	function Window:close_category()
 		if #opened_categories <= 1 then return end 
 		table.remove(opened_categories)
-		
+
 		local previous = opened_categories[#opened_categories]
 		if not previous then return end
-		
+
 		local current_category = Window.current_category
-		
+
 		current_category:SetVisible(false)
 		previous:SetVisible(true)
-		
+
 		Window:resize(previous)
 		Window.current_category = previous
 	end
-	
+
 	function Window:resize(category)
 		main_frame.Size = UDim2.new(0, 300, 0, 70 + category.size * 35)
 	end
-	
+
 	UserInputService.InputBegan:Connect(function(input)
 		local key = input.KeyCode
 		local category = Window.current_category
@@ -364,13 +371,13 @@ function Library:create_window(title, icon, version)
 			Window:close_category()
 		end
 	end)
-	
+
 	local ContextActionService = game:GetService("ContextActionService")
 
 	ContextActionService:BindActionAtPriority("DisableArrowKeys", function()
 		return Enum.ContextActionResult.Sink
 	end, false, Enum.ContextActionPriority.High.Value, Enum.KeyCode.Up, Enum.KeyCode.Down, Enum.KeyCode.Left, Enum.KeyCode.Right)
-	
+
 	return Window	
 end
 
